0c8c75a900f0754160053caa190f5ca0848ca272
user <user@DESKTOP-RN36T6C>
2025-12-04 21:33:52 +0200

    Add project files.

---
+++ Turtle/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Turtle.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
